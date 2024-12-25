26e1aaf1082af30a715ea3fe6b995df622dc91d6
hb1h <35057925+hb1h@users.noreply.github.com>
2024-12-26 03:27:25 +0500

    Add files via upload

---
+++ src/assets/index.ts
@@ -0,0 +1,69 @@
+import logo1 from "./b-logo1.png";
+import logo2 from "./b-logo2.png";
+import logo3 from "./b-logo3.png";
+import logo4 from "./b-logo4.png";
+import logo5 from "./b-logo5.png";
+import icon1 from "./sr-icon1.png";
+import hicon1 from "./sr-icon1-hover.png";
+import icon2 from "./sr-icon2.png";
+import hicon2 from "./sr-icon2-hover.png";
+import icon3 from "./sr-icon3.png";
+import hicon3 from "./sr-icon3-hover.png";
+import icon4 from "./sr-icon4.png";
+import hicon4 from "./sr-icon4-hover.png";
+import aboutposter from "./about_img_02.png";
+import textLogo from "./sub-text-logo.png";
+import protfolio1 from "./protfolio-img01.png";
+import protfolio2 from "./protfolio-img02.png";
+import protfolio3 from "./protfolio-img03.png";
+import protfolio4 from "./protfolio-img04.png";
+import about4 from "./about_img_04.png";
+import avatar1 from "./avatar1.png";
+import avatar2 from "./avatar2.png";
+import stars from "./review-icon.png";
+import clientLogo1 from "./t-logo.png";
+import clientLogo2 from "./t-logo2.png";
+import qtIcon from "./qt-icon.png";
+import patternbg from "./sr-h-bg.png";
+import cpLogo from "./cp-logo.png";
+import logo from "./logo.png";
+import email from "./email.png";
+import member from "./member.png";
+import bannerBg from "./bannerBg.png";
+import connectingPeople from "./connectingPeople.jpg";
+
+export const ICON = {
+  logo,
+  logo1,
+  logo2,
+  logo3,
+  logo4,
+  logo5,
+  textLogo,
+  icon1,
+  hicon1,
+  icon2,
+  hicon2,
+  icon3,
+  hicon3,
+  icon4,
+  hicon4,
+  aboutposter,
+  protfolio1,
+  protfolio2,
+  protfolio3,
+  protfolio4,
+  about4,
+  avatar1,
+  avatar2,
+  stars,
+  clientLogo1,
+  clientLogo2,
+  qtIcon,
+  patternbg,
+  cpLogo,
+  email,
+  member,
+  bannerBg,
+  connectingPeople
+};
